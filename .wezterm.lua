c7e17150b160e2b7e91d8a5db4a4bc036397790d
Marc Wilson <posguy99@gmail.com>
2024-11-03 17:12:54 -0800

    wezterm - disable automatic update checks via api.github.com on startup

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,6 +13,10 @@ local config = {}
     config.initial_cols = 120
     config.initial_rows= 36
 
+    -- https://wezfurlong.org/wezterm/config/lua/config/check_for_updates.html
+    -- https://github.com/wez/wezterm/issues/6355#issuecomment-2453465060
+    config.check_for_updates = false
+
     -- https://wezfurlong.org/wezterm/config/lua/config/hide_tab_bar_if_only_one_tab.html
     config.hide_tab_bar_if_only_one_tab = true
 
@@ -20,5 +24,22 @@ local config = {}
     -- itself.  think "what happens if you type "exit" at a shell prompt.
     -- https://wezfurlong.org/wezterm/config/lua/config/exit_behavior.html
     config.exit_behavior = 'CloseOnCleanExit'
+
+    -- https://wezfurlong.org/wezterm/config/lua/config/window_close_confirmation.html
+    config.window_close_confirmation = 'NeverPrompt'
+
+    -- https://wezfurlong.org/wezterm/config/lua/config/skip_close_confirmation_for_processes_named.html
+    config.skip_close_confirmation_for_processes_named = {
+        'bash',
+        'sh',
+        'zsh',
+        'fish',
+        'ksh',
+        'tmux',
+        'nu',
+        'cmd.exe',
+        'pwsh.exe',
+        'powershell.exe',
+    }
 
 return config
